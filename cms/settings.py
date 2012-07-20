170a10bf4e6a03ea546b5f8276f45714818eb441
Ronnie B. Beltran <rbbeltran.09@gmail.com>
2012-07-20 20:03:48 +0800

    add site title and description in settings.py

--- cms/settings.py
+++ cms/settings.py
@@ -132,6 +132,9 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
+SITE_TITLE = 'Django'
+SITE_DESCRIPTION = 'Django is a high-level Python Web framework that encourages rapid development and clean, pragmatic design'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
