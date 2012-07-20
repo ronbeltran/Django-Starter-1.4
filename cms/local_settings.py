756adf7b3c40ea3316ef17e3be3bcc157dd0e5d0
Ronnie B. Beltran <rbbeltran.09@gmail.com>
2012-07-20 19:57:55 +0800

    remove local_settings.py in .gitignore

---
+++ cms/local_settings.py
@@ -0,0 +1,22 @@
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'cms.sqlite3',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'development-key'
+
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+
+if DEBUG:
+    # Show emails in the console during development.
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
