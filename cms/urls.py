bf7d087ea9e93b6390349bae68eb65b9cf31e091
Ronnie B. Beltran <rbbeltran.09@gmail.com>
2012-07-24 22:18:34 +0800

    add homepage view

--- cms/urls.py
+++ cms/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'cms.views.home', name='home'),
+    url(r'^$', 'cms.views.home', name='home'),
     # url(r'^cms/', include('cms.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
---
+++ cms/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def home(request):
+    variables = RequestContext(request, {
+    })
+    return render_to_response(
+        'home.html', variables,
+    ) 
